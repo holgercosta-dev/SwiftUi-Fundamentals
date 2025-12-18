34888fb8eb47eeeecd3ab4ca52c424f4a8f9e8c8
rogerknowws <holgerxmxcosta.dev@gmail.com>
2025-12-18 15:41:47 -0100

    Apple-Frameworks-Added image assets.

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,18 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Roger Knowws on 18/12/2025.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+#Preview {
+    FrameworkGridView()
+}
