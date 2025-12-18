45263fbcc6b4a792a180bb106b2d9ea8b2a79f20
rogerknowws <holgerxmxcosta.dev@gmail.com>
2025-12-18 15:20:06 -0100

    Created new project Apple-Frameworks

---
+++ Apple-Frameworks/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Roger Knowws on 18/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
