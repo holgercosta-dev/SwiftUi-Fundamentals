509f5c8284ff3010ccbf18da4e0b847f0850d6d5
rogerknowws <holgerxmxcosta.dev@gmail.com>
2025-12-19 13:46:07 -0100

    GhibliApp-Created sample data and corresponding model stucts. Created main view for showing list of films.

---
+++ GhibliApp/GhibliApp/Views/FilmListView.swift
@@ -0,0 +1,18 @@
+//
+//  FilmListView.swift
+//  GhibliApp
+//
+//  Created by Roger Knowws on 19/12/2025.
+//
+
+import SwiftUI
+
+struct FilmListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FilmListView()
+}
