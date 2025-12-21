3b35b8106d45057bf8b16c11fd9760907328ef15
rogerknowws <holgerxmxcosta.dev@gmail.com>
2025-12-21 20:21:46 -0100

    FitnessApp-Created project.

---
+++ FitnessApp/FitnessApp/FitnessAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitnessAppApp.swift
+//  FitnessApp
+//
+//  Created by Roger Knowws on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct FitnessAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
