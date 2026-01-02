1cc7d0d414c0b42bc0f47f93a77a8f126e40baa1
rogerknowws <holgerxmxcosta.dev@gmail.com>
2026-01-02 20:46:30 -0100

    FitnessApp-Added basic ui for list.

--- FitnessApp/FitnessApp/Presentation/Leaderboard/LeaderboardScreen.swift
+++ FitnessApp/FitnessApp/Presentation/Leaderboard/LeaderboardScreen.swift
@@ -7,9 +7,55 @@
 
 import SwiftUI
 
+struct LeaderboardUser: Codable, Identifiable {
+    var id: UUID = UUID()
+    let createdAt: String
+    let username: String
+    let count: Int
+    
+    static var sampleData = [
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+        LeaderboardUser(createdAt: "", username: "name", count: 1234),
+    ]
+}
+
 struct LeaderboardScreen: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text("Leaderboard")
+                .font(.largeTitle)
+                .bold()
+            
+            HStack {
+                Text("Name")
+                    .bold()
+            
+                Spacer()
+                
+                Text("Steps")
+                    .bold()
+            }
+            .padding()
+            
+            LazyVStack {
+                ForEach(LeaderboardUser.sampleData) { user in
+                    HStack {
+                        Text(user.username)
+                        
+                        Spacer()
+                        
+                        Text("\(user.count)")
+                    }
+                    .padding(.horizontal)
+                }
+            }
+        }
+        
+        Spacer()
     }
 }
 
